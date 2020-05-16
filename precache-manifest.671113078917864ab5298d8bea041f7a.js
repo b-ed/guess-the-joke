ac90d13dbd59429e48b29d3cf375740d5537176b
b-ed <67429249+b-ed@users.noreply.github.com>
2020-05-17 00:50:27 +0300

    Updates

---
+++ precache-manifest.671113078917864ab5298d8bea041f7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d4a5bbf2324e599679763f8388fd84b",
+    "url": "/guess-the-joke/index.html"
+  },
+  {
+    "revision": "30c3e010c9bdceec8b3b",
+    "url": "/guess-the-joke/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3e8a7fdc04ebe761dcb7",
+    "url": "/guess-the-joke/static/css/main.ae8240aa.chunk.css"
+  },
+  {
+    "revision": "30c3e010c9bdceec8b3b",
+    "url": "/guess-the-joke/static/js/2.d567dc85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/guess-the-joke/static/js/2.d567dc85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e8a7fdc04ebe761dcb7",
+    "url": "/guess-the-joke/static/js/main.33fe8532.chunk.js"
+  },
+  {
+    "revision": "523082f55cf2e10d3d35",
+    "url": "/guess-the-joke/static/js/runtime-main.67ad1c0d.js"
+  }
+]);
